da59a3de3a6e5a72073a71893aa9c546585a4393
BreezeJuvenile <202309057@huat.edu.cn>
2024-10-20 20:52:57 +0800

    change commit

--- test11.c
+++ test11.c
@@ -6,13 +6,13 @@
 int main() {
 	int Five = 0, Two = 0, One = 0, n = 0;
 	scanf("%d", &n);
-	printf("¿ªÊ¼±íÑÝ\n");
+
 	for (Five = 1; Five <= SUM; Five++) {
 		for (Two = 1; Two < SUM; Two++) {
 			for (One = 1; One < SUM; One++) {
 				if ((Five * 5 + Two * 2 + One * 1 == 150) && (Five + Two + One == 100) && n) {
 					printf("%d %d %d \n", Five, Two, One);
-					printf("%d %d %d \n", Five, Two, One);
+
 					n--;
 				}
 			}
